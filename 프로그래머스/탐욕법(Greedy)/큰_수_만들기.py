125d63acedb35b350b55fd7774880794f08122c3
sungchan1 <sungchan254@naver.com>
2022-01-26 22:48:31 +0900

    A leetcode a day keeps unemployment away

--- 프로그래머스/탐욕법(Greedy)/큰_수_만들기.py
+++ 프로그래머스/탐욕법(Greedy)/큰_수_만들기.py
@@ -1,44 +1,68 @@
 def solution(number, k):
-    
-    str_list = list(number)
-    
-    str_length = len(number)-1
-    stack_list = ['A'] * (str_length+1) 
-    index = 0
-    stack_index = 0
-    for str_number in str_list :
-        if str_number == '0' :
-            index +=1
+    stack_list = list()
+    for num in number :
+        # print(num)
+        if not stack_list :
+            # print("push ", num)
+            stack_list.append(num)
         else :
-            break
-    if index != 0:
-        str_list = str_list[index:]
-    stack_list[0] = str_list[0]
-    # print("push 1 :", str_list[0])
-    index = 1 
-    while True :
-        if  stack_list[stack_index] >= str_list[index] : # 처음올때 stack_index = 0, index = 1
-            stack_list[stack_index+1] = str_list[index]
-            # print("push 2 :", str_list[index])
-            if index == str_length: #끝까지 왔을 떄
-                del str_list[index]
-                index = 0
-                k -= 1
-                if k == 0 :
+            while True:
+                if k> 0 and stack_list and stack_list[-1] <num :
+                    # print("pop ", stack_list[-1])
+                    stack_list.pop(-1)
+                    k -= 1
+                else :
+                    # print("push ", num)
+                    stack_list.append(num)
                     break
-                str_length -= 1
-                stack_index = -1
-                stack_list[0] = str_list[0]
-                # print("push 3 :", str_list[index])
-            index +=1
-            stack_index += 1
-        else : # 뒤에 들어온 수가 스택에 수보다 클때
-            # print("pop", str_list[index-1], " pre ", stack_list[stack_index], " after ", str_list[index])
-            del str_list[index-1]
-            k -= 1
-            if k == 0 :
-                break
-            index -=1
-            str_length -=1
-            stack_index -=1
-    return "".join(str_list)
+    if k > 0 :
+        for i in range(k):
+            stack_list.pop(-1)
+        # print("stack_list ", stack_list)
+    answer = "".join(stack_list)
+    return answer
+
+# def solution(number, k):
+    
+#     str_list = list(number)
+    
+#     str_length = len(number)-1
+#     stack_list = ['A'] * (str_length+1) 
+#     index = 0
+#     stack_index = 0
+#     for str_number in str_list :
+#         if str_number == '0' :
+#             index +=1
+#         else :
+#             break
+#     if index != 0:
+#         str_list = str_list[index:]
+#     stack_list[0] = str_list[0]
+#     # print("push 1 :", str_list[0])
+#     index = 1 
+#     while True :
+#         if  stack_list[stack_index] >= str_list[index] : # 처음올때 stack_index = 0, index = 1
+#             stack_list[stack_index+1] = str_list[index]
+#             # print("push 2 :", str_list[index])
+#             if index == str_length: #끝까지 왔을 떄
+#                 del str_list[index]
+#                 index = 0
+#                 k -= 1
+#                 if k == 0 :
+#                     break
+#                 str_length -= 1
+#                 stack_index = -1
+#                 stack_list[0] = str_list[0]
+#                 # print("push 3 :", str_list[index])
+#             index +=1
+#             stack_index += 1
+#         else : # 뒤에 들어온 수가 스택에 수보다 클때
+#             # print("pop", str_list[index-1], " pre ", stack_list[stack_index], " after ", str_list[index])
+#             del str_list[index-1]
+#             k -= 1
+#             if k == 0 :
+#                 break
+#             index -=1
+#             str_length -=1
+#             stack_index -=1
+#     return "".join(str_list)
