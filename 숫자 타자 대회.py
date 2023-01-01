78aad5e4940e70e41b3e1c79b68fe23b9b3a297c
sungchan1 <sungchan254@naver.com>
2023-01-01 23:49:47 +0900

    A leetcode a day keeps unemployment away
    A leetcode a day keeps unemployment away

---
+++ 숫자 타자 대회.py
@@ -0,0 +1,25 @@
+
+
+def solution(numbers):
+    def search(left, right, index, total):
+        if len(numbers) == index:
+            return total
+        if left == right:
+            return float('inf')
+        number = int(numbers[index])
+        left_total = search(number, right, index + 1, total) + weights[left][number]
+        right_total = search(left, number, index + 1, total) + weights[right][number]
+        return min(left_total, right_total)
+
+
+    weights = ((1, 7, 6, 7, 5, 4, 5, 3, 2, 3),
+               (7, 1, 2, 4, 2, 3, 5, 4, 5, 6),
+               (6, 2, 1, 2, 3, 2, 3, 5, 4, 5),
+               (7, 4, 2, 1, 5, 3, 2, 6, 5, 4),
+               (5, 2, 3, 5, 1, 2, 4, 2, 3, 5),
+               (4, 3, 2, 3, 2, 1, 2, 3, 2, 3),
+               (5, 5, 3, 2, 4, 2, 1, 5, 3, 2),
+               (3, 4, 5, 6, 2, 3, 5, 1, 2, 4),
+               (2, 5, 4, 5, 3, 2, 3, 2, 1, 2),
+               (3, 6, 5, 4, 5, 3, 2, 4, 2, 1))
+    return search(4, 6, 0, 0)
