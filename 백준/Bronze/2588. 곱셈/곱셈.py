77af8d18503188d64f19a19f358b94353d817d1a
sungchan Yeo <sungchan254@naver.com>
2024-01-18 23:58:18 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,11 @@
+value1 = int(input())
+value2 = int(input())
+number1 = value2 % 10
+number2 = (value2 % 100) // 10
+number3 = value2 // 100
+
+print(value1 * number1)
+print(value1 * number2)
+print(value1 * number3)
+print(value1 * value2)
+
