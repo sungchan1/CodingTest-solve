5bfc744ef2b2e6fc2691b84c12dedfb14f34985d
sungchan Yeo <sungchan254@naver.com>
2024-01-20 13:41:15 +0900

    [Bronze I] Title: 약수들의 합, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -4,20 +4,18 @@ while True:
     number = int(input())
     if number == -1:
         break
-
-    # 약수들을 저장할 리스트
-    divisors = []
-
-    # 약수들을 찾기
-    for i in range(1, int(math.sqrt(number)) + 1):
-        if number % i == 0:
-            divisors.append(i)
-            if i != 1 and i != number // i:
-                divisors.append(number // i)
-
-    # 약수들의 합이 주어진 수와 같은지 확인
-    if sum(divisors) == number:
-        divisors.sort()  # 약수들을 오름차순으로 정렬
-        print("{0} = {1}".format(number, " + ".join(map(str, divisors))))
-    else:
+    result = 0
+    divisor = set()
+    for count in range(1, int(math.sqrt(number)+1)):
+        if number % count == 0:
+            divisor.add(count)
+            divisor.add(int(number / count))
+    divisor = list(divisor)
+    divisor.sort()
+    if sum(divisor) == number *2 :
+        divisor.remove(number)
+        print("{0} = {1}".format(number, " + ".join(map(str, divisor))))
+    else :
         print("{0} is NOT perfect.".format(number))
+
+
