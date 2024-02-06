7c1223bb66bc0e1d7a343b564329bad67b8351b5
sungchan Yeo <sungchan254@naver.com>
2024-02-06 17:57:25 +0900

    [Silver III] Title: N과 M (3), Time: 864 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,13 @@
+from sys import stdin as s
+def dfs(numbers, n, m) -> None:
+    if len(numbers) == m:
+        print(" ".join(numbers))
+        return
+
+    for number in range(1, n+1):
+        # if not str(number) in numbers:
+        dfs(numbers + str(number), n, m)
+
+# s = open("input.txt", "r")
+n, m = map(int, s.readline().split())
+dfs("", n, m)
