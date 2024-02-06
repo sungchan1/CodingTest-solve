6951390e997ed49387da1688eb2cba698d1a8a96
sungchan Yeo <sungchan254@naver.com>
2024-02-06 13:35:33 +0900

    [Silver III] Title: Four Squares, Time: 188 ms, Memory: 111112 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -4,18 +4,15 @@ from sys import stdin as s, stdout as o
 
 n = int(s.readline())
 result = 0
-lagrange_numbers = [float("inf")] * (n+1)
-
+lagrange_numbers = [4] * (n+1)
+squares = [i**2 for i in range(int(n**0.5)+1)]
 for i in range(1, n+1):
     k = int(i ** 0.5)
     if k**2 == i :
         lagrange_numbers[i] = 1
         continue
     for j in range(k, 0, -1):
-        # print(i, k, j, i-j**2)
-        lagrange_numbers[i] = min(lagrange_numbers[i], lagrange_numbers[i-j**2]+1)
+        lagrange_numbers[i] = min(lagrange_numbers[i], lagrange_numbers[i-squares[j]]+1)
         if lagrange_numbers[i] == 2:
             break
 print(lagrange_numbers[n])
-
-
