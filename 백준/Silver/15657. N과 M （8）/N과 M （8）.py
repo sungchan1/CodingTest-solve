39cd54c8f7b2dce424a4d90b0b5362b4ab9e5c83
sungchan Yeo <sungchan254@naver.com>
2024-02-06 18:14:11 +0900

    [Silver III] Title: N과 M (8), Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,16 @@
+from sys import stdin as s
+
+def dfs(input_numbers, numbers, n, m) -> None:
+    if len(numbers) == m:
+        print(" ".join(map(str, numbers)))
+        return
+
+    for index, number in enumerate(input_numbers):
+        # if not number in numbers:
+        if not numbers or int(numbers[-1]) <= number:
+            dfs(input_numbers, numbers[:] + [number], n, m)
+
+# s = open("input.txt", "r")
+n, m = map(int, s.readline().split())
+input_numbers = sorted(list(map(int, s.readline().split())))
+dfs(input_numbers,[], n, m)
