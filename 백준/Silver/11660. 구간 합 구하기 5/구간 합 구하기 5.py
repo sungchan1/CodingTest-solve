1ec1df3a81f82a5af9c079090ff684a88a81c4a0
sungchan Yeo <sungchan254@naver.com>
2024-02-09 16:38:12 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1128 ms, Memory: 106040 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -11,12 +11,10 @@ for row in range(n):
 
 for row in range(1, n+1):
     for col in range(1, n+1):
-        dp[row][col] = dp[row][col-1] + numbers[row-1][col-1]
+        dp[row][col] = dp[row-1][col] + dp[row][col-1] - dp[row-1][col-1] + numbers[row-1][col-1]
 
 for test in range(m):
     x1, y1, x2, y2 = map(int, s.readline().split())
-    result = 0
-    for row in range(x1, x2+1):
-        result += dp[row][y2] - dp[row][y1-1]
+    result = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]
     print(result)
 
