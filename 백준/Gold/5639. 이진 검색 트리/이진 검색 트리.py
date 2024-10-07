3f55ec23cc01192837f712ad1e486da8d428b6ae
sungchan Yeo <sungchan254@naver.com>
2024-10-07 23:56:05 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 1428 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -2,11 +2,11 @@ import sys
 
 sys.setrecursionlimit(10 ** 6)
 
-# 입력받기
-preorder = []
+
+stack = []
 while True:
     try:
-        preorder.append(int(input()))
+        stack.append(int(input()))
     except:
         break
 
@@ -38,7 +38,6 @@ def insert_iter(root, key):
         parent.right = new_node
 
 
-# 후위 순회 (반복문 사용)
 def postorder_iter(root):
     stack = []
     last_visited = None
@@ -58,17 +57,18 @@ def postorder_iter(root):
                 result.append(peek_node.key)
                 last_visited = stack.pop()
 
-    # 후위 순회 결과 출력
+
     for value in result:
         print(value)
 
 
-# 트리 생성
-if preorder:
-    root = Node(preorder[0])  # 첫 번째 값으로 루트 노드 생성
 
-    for key in preorder[1:]:
+
+if stack:
+    root = Node(stack[0])
+
+    for key in stack[1:]:
         insert_iter(root, key)
 
-    # 후위 순회 출력
+
     postorder_iter(root)
