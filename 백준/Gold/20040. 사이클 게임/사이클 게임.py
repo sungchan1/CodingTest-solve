248d91779913c4f02a7d0419b1982efb7647b657
sungchan Yeo <sungchan254@naver.com>
2024-11-16 12:40:57 +0900

    [Gold IV] Title: 사이클 게임, Time: 824 ms, Memory: 50548 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,53 @@
+from sys import stdin
+
+n, m = map(int, stdin.readline().split())
+
+points = [i for i in range(n)]
+
+
+def union(p1, p2):
+    parent1 = find(p1)
+    parent2 = find(p2)
+
+    if parent1 > parent2:
+        parent1, parent2 = parent2, parent1
+
+    points[parent2] = parent1
+    points[p1] = parent1
+    points[p2] = parent1
+
+
+
+def find(point):
+    p = point
+
+    paths = []
+    while p != points[p]:
+        paths.append(p)
+        p = points[p]
+
+    parent = p
+
+    for path in paths:
+        points[path] = parent
+
+    return parent
+
+
+result = 0
+
+for count in range(1, m + 1):
+    p1, p2 = map(int, stdin.readline().split())
+
+
+    if find(p1) == find(p2):
+        result = count
+        break
+
+    union(p1, p2)
+
+
+
+
+
+print(result)
