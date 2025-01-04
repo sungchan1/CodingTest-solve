e650d73a3f7553229ff77d8c6f319cdac65a3f0e
sungchan Yeo <sungchan254@naver.com>
2025-01-04 18:09:54 +0900

    [Gold V] Title: 평범한 배낭, Time: 2436 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,24 +1,15 @@
-
 N, K = map(int, input().split())
 
-
 items = [
-	list(map(int, input().split()))
-	for i in range(N)
+    list(map(int, input().split()))
+    for _ in range(N)
 ]
 
+dp = [0] * (K + 1)  # 1차원 DP 배열
 
-dp = [ [0] * (K+1) for _ in range(N+1)]
+for weight, value in items:
+    # 무게를 K부터 weight까지 거꾸로 탐색 (값이 덮어쓰이지 않도록)
+    for w in range(K, weight - 1, -1):
+        dp[w] = max(dp[w], dp[w - weight] + value)
 
-for i in range(1,N+1):
-	for w in range(1,K+1):
-		weight = items[i-1][0]
-		value = items[i-1][1]
-
-		if weight > w:
-			dp[i][w] = dp[i-1][w]
-		else:
-			dp[i][w] = max(dp[i-1][w], dp[i-1][w-weight] + value)
-
-
-print(dp[N][K])
+print(dp[K])
