395a5e6383f154fe85ebeb63800d64911c6040de
sungchan Yeo <sungchan254@naver.com>
2025-03-28 10:47:01 +0900

    [Platinum V] Title: 광부 호석, Time: 580 ms, Memory: 107448 KB, Score: 15 point -BaekjoonHub

--- 백준/Platinum/21279. 광부 호석/광부 호석.py
+++ 백준/Platinum/21279. 광부 호석/광부 호석.py
@@ -1,28 +1,43 @@
-from collections import deque
-import heapq
 import sys
-
-
+from collections import defaultdict
 input = sys.stdin.readline
-n, c = map(int, input().split())
-gems = [list(map(int, input().split())) for _ in range(n)]
-gems.sort()
+n, cash = map(int, input().split())
 
-info = []
 
-total = 0
-ans = 0
+x_gems = defaultdict(list)
+y_gems = defaultdict(list)
 
-for w, h, v in gems:
-    c -= 1
-    total += v
-    heapq.heappush(info, [-h, v])
-    while c < 0:
-        _, prev_v = heapq.heappop(info)
-        c += 1
-        total -= prev_v
+for _ in range(n):
+    x, y, v = map(int, input().split())
+    x_gems[x].append((y, v))
+    y_gems[y].append((x, v))
 
-    ans = max(ans, total)
+cost = 0
+result = 0
+value = 0
+x = 100_000
+y = 0
 
-print(ans)
+while x >= 0 and y <= 100_000:
+    if cost <= cash:
+        result = max(result, value)
+
+        for _x, v in y_gems[y]:
+            if _x <= x:
+                cost += 1
+                value += v
+        y += 1
+    else:
+        for _y, v in x_gems[x]:
+            if _y <= y:
+                cost -= 1
+                value -= v
+
+        x -= 1
+
+    if cost <= cash:
+        result = max(result, value)
+
+
+print(result)
 
